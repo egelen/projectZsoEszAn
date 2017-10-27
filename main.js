19df632301d1666407b34a08f67b9d0039484f51
Eszter Gelencsér <eszter.gelencser1@gmail.com>
2017-10-27 13:51:20 +0200

    taknyolagat

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ function getJson(url, callback) {
   var xhr = new XMLHttpRequest();
   xhr.open("get", url);
 
-  xhr.onload = function(ev) {
+  xhr.onload = function (ev) {
     var footballData = JSON.parse(ev.target.response);
     callback(footballData);
   };
--- match.js
+++ match.js
@@ -1,11 +1,11 @@
 var code = location.href.split("=")[1];
 getJson(
-    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/en.1.json",
-    function(tableData) {
+    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/it.1.json",
+    function (tableData) {
         fillTable(tableData);
     }
 );
 
 function fillTable(rows) {
-    // 
+
 }
