1d6a38d31f2b11593a3bba942a2f84d63b0da8b6
StarHank <27526884+StarHank@users.noreply.github.com>
2017-10-27 13:54:35 +0200

    Add files via upload
    ez egy félhülye match.js

--- match.js
+++ match.js
@@ -1,11 +1,26 @@
 var code = location.href.split("=")[1];
 getJson(
-    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/en.1.json",
-    function(tableData) {
-        fillTable(tableData);
+    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/it.1.json",
+    function (tableData) {
+        fillTable(tableData.rounds[0].matches);
     }
 );
 
 function fillTable(rows) {
-    // 
+    console.log(rows);
+    var table = document.querySelector("table");
+    var content = "";
+    for (var i = 0; i < rows.length; i++) {
+        var tr = "<tr>";
+        tr += "<td>" + (i + 1) + "</td>";
+        tr += "<td>" + rows[i].key + "</td>";
+        tr += "<td>" + rows[i].name + "</td>";
+        tr += "<td>" + rows[i].date + "</td>";
+        tr += '<td><a class="btn btn-primary" href="match.html?code=' + rows[i].code + '">match</a></td>';
+        tr += "</tr>";
+        content += tr;
+
+
+    }
+    table.querySelector("tbody").innerHTML = content;
 }
