4b8b47b13b1af9de9d4214ef665d4f04a92eb059
StarHank <27526884+StarHank@users.noreply.github.com>
2017-10-27 13:46:17 +0200

    Add files via upload
    feat: a match.js behívja a megfelelő jsont

--- ZsoEszAn/bestYellow/js/match.js
+++ ZsoEszAn/bestYellow/js/match.js
@@ -2,10 +2,12 @@ var code = location.href.split("=")[1];
 getJson(
     "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/it.1.json",
     function (tableData) {
-        fillTable(tableData);
+        fillTable(tableData.rounds[0].matches);
     }
 );
 
 function fillTable(rows) {
-    // 
+    console.log(rows);
+
+
 }
