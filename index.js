19dc20d5125702570a511e5ee1802bd5dd2ae61c
Zita Zsófia Zagyva <zagyvazsofi@gmail.com>
2017-10-29 20:16:27 +0100

    keresés

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var tableData = [];
 
 getJson(
-    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/en.1.clubs.json",
-    function(data) {
+    "https://raw.githubusercontent.com/opendatajson/football.json/master/2017-18/it.1.clubs.json",
+    function (data) {
         tableData = data.clubs;
         fillTable(tableData);
     }
@@ -11,13 +11,13 @@ getJson(
 function fillTable(rows) {
     var table = document.querySelector("table");
     var content = "";
-    for (var i = 0; i < rows.length; i++ ) {
+    for (var i = 0; i < rows.length; i++) {
         var tr = "<tr>";
-        tr += "<td>"+(i+1)+"</td>";
-        tr += "<td>"+rows[i].key+"</td>";
-        tr += "<td>"+rows[i].name+"</td>";
-        tr += "<td>"+rows[i].code+"</td>";
-        tr += '<td><a class="btn btn-primary" href="match.html?code='+rows[i].code+'">match</a></td>';
+        tr += "<td>" + (i + 1) + "</td>";
+        tr += "<td>" + rows[i].key + "</td>";
+        tr += "<td>" + rows[i].name + "</td>";
+        tr += "<td>" + rows[i].code + "</td>";
+        tr += '<td><a class="btn btn-primary" href="match.html?code=' + rows[i].code + '">match</a></td>';
         tr += "</tr>";
         content += tr;
     }
@@ -25,21 +25,9 @@ function fillTable(rows) {
     table.querySelector("tbody").innerHTML = content;
 }
 
-var kacsak = [
-    {
-        name: "Hápi",
-        code: "hápi",
-        key: "hpi"
-    },
-    {
-        name: "Sápi",
-        code: "sápi",
-        key: "spi"
-    }
-];
 
-document.querySelector("#sort-btn").addEventListener("click", function() {
-    tableData.sort( function(a, b) {
+document.querySelector("#btn-sort").addEventListener("click", function () {
+    tableData.sort(function (a, b) {
         if (!a.code) {
             return -1;
         } else if (!b.code) {
@@ -49,3 +37,16 @@ document.querySelector("#sort-btn").addEventListener("click", function() {
     });
     fillTable(tableData);
 });
+
+function searchFunction() {
+    var search =
+        document.getElementById("search").placeholder;
+    var clubs = document.getElementById("tableOfClubs");
+    for (i in search) {
+        if (i = clubs) {
+            return clubs;
+
+        }
+    }
+
+}
